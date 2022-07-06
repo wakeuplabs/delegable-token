5032d059e28135fb447f93d61b4249577a9cfbdb
Christian Escalante <christian@ext.iovlabs.org>
2022-07-06 15:23:40 -0300

    feat: split delegable logic and added wakeup logo

--- contracts/DelegableToken.sol
+++ contracts/DelegableToken.sol
@@ -1,11 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+//
+//  /$$      /$$           /$$                 /$$   /$$
+// | $$  /$ | $$          | $$                | $$  | $$
+// | $$ /$$$| $$  /$$$$$$ | $$   /$$  /$$$$$$ | $$  | $$  /$$$$$$
+// | $$/$$ $$ $$ |____  $$| $$  /$$/ /$$__  $$| $$  | $$ /$$__  $$
+// | $$$$_  $$$$  /$$$$$$$| $$$$$$/ | $$$$$$$$| $$  | $$| $$  \ $$
+// | $$$/ \  $$$ /$$__  $$| $$_  $$ | $$_____/| $$  | $$| $$  | $$
+// | $$/   \  $$|  $$$$$$$| $$ \  $$|  $$$$$$$|  $$$$$$/| $$$$$$$/
+// |__/     \__/ \_______/|__/  \__/ \_______/ \______/ | $$____/
+//                                                      | $$
+//                                                      | $$
+//
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "./ERC4907Extension.sol";
+import "./DelegableTokenExtension.sol";
 
-contract DelegableToken is ERC4907Extension {
+contract DelegableToken is DelegableTokenExtension {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -13,58 +25,21 @@ contract DelegableToken is ERC4907Extension {
     uint256 public _maxSupply = 0;
     string public _baseUri;
 
-    bool public _allowTransferBeforeUserExpired;
-    bool public _allowChangeUserBeforeUserExpired;
-
     constructor(
         string memory baseUri,
         uint256 price,
         uint256 maxSupply,
         bool allowTransferBeforeUserExpired,
         bool allowChangeUserBeforeUserExpired
-    ) ERC4907Extension("DelegableToken", "DT721") {
+    )
+        DelegableTokenExtension(
+            allowTransferBeforeUserExpired,
+            allowChangeUserBeforeUserExpired
+        )
+    {
         _price = price;
         _maxSupply = maxSupply;
         _baseUri = baseUri;
-
-        _allowTransferBeforeUserExpired = allowTransferBeforeUserExpired;
-        _allowChangeUserBeforeUserExpired = allowChangeUserBeforeUserExpired;
-    }
-
-    function changeConfiguration(
-        bool allowTransferBeforeUserExpired,
-        bool allowChangeUserBeforeUserExpired
-    ) external onlyOwner {
-        _allowTransferBeforeUserExpired = allowTransferBeforeUserExpired;
-        _allowChangeUserBeforeUserExpired = allowChangeUserBeforeUserExpired;
-    }
-
-    function setUser(
-        uint256 tokenId,
-        address user,
-        uint256 expires
-    ) public virtual override {
-        require(
-            _allowChangeUserBeforeUserExpired ||
-                this.userOf(tokenId) == address(0),
-            "token not available"
-        );
-
-        super.setUser(tokenId, user, expires);
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal virtual override {
-        require(
-            _allowTransferBeforeUserExpired ||
-                this.userOf(tokenId) == address(0),
-            "token not available"
-        );
-
-        super._beforeTokenTransfer(from, to, tokenId);
     }
 
     // @notice Mints new NFT to msg.sender
---
+++ contracts/DelegableTokenExtension.sol
@@ -0,0 +1,67 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+//
+//  /$$      /$$           /$$                 /$$   /$$
+// | $$  /$ | $$          | $$                | $$  | $$
+// | $$ /$$$| $$  /$$$$$$ | $$   /$$  /$$$$$$ | $$  | $$  /$$$$$$
+// | $$/$$ $$ $$ |____  $$| $$  /$$/ /$$__  $$| $$  | $$ /$$__  $$
+// | $$$$_  $$$$  /$$$$$$$| $$$$$$/ | $$$$$$$$| $$  | $$| $$  \ $$
+// | $$$/ \  $$$ /$$__  $$| $$_  $$ | $$_____/| $$  | $$| $$  | $$
+// | $$/   \  $$|  $$$$$$$| $$ \  $$|  $$$$$$$|  $$$$$$/| $$$$$$$/
+// |__/     \__/ \_______/|__/  \__/ \_______/ \______/ | $$____/
+//                                                      | $$
+//                                                      | $$
+//                                                      |__/
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./ERC4907Extension.sol";
+
+abstract contract DelegableTokenExtension is ERC4907Extension {
+    bool public _allowTransferBeforeUserExpired;
+    bool public _allowChangeUserBeforeUserExpired;
+
+    constructor(
+        bool allowTransferBeforeUserExpired,
+        bool allowChangeUserBeforeUserExpired
+    ) ERC4907Extension("DelegableToken", "DT721") {
+        _allowTransferBeforeUserExpired = allowTransferBeforeUserExpired;
+        _allowChangeUserBeforeUserExpired = allowChangeUserBeforeUserExpired;
+    }
+
+    function changeConfiguration(
+        bool allowTransferBeforeUserExpired,
+        bool allowChangeUserBeforeUserExpired
+    ) external onlyOwner {
+        _allowTransferBeforeUserExpired = allowTransferBeforeUserExpired;
+        _allowChangeUserBeforeUserExpired = allowChangeUserBeforeUserExpired;
+    }
+
+    function setUser(
+        uint256 tokenId,
+        address user,
+        uint256 expires
+    ) public virtual override {
+        require(
+            _allowChangeUserBeforeUserExpired ||
+                this.userOf(tokenId) == address(0),
+            "token not available"
+        );
+
+        super.setUser(tokenId, user, expires);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override {
+        require(
+            _allowTransferBeforeUserExpired ||
+                this.userOf(tokenId) == address(0),
+            "token not available"
+        );
+
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+}
