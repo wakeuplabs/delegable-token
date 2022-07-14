01706f8998b8c0c3e9644860fbb336268bdc1999
WakeUp Labs <97481473+wakeupdao@users.noreply.github.com>
2022-07-14 09:19:11 -0300

    Fix Watermark

--- contracts/DelegableToken.sol
+++ contracts/DelegableToken.sol
@@ -5,7 +5,8 @@ pragma solidity ^0.8.0;
 //    \ \ /\ / / _` | |/ / _ \ | | | '_ \
 //     \ V  V / (_| |   <  __/ |_| | |_) |
 //      \_/\_/ \__,_|_|\_\___|\___/| .__/
-//       2022                      |_|
+//                                 |_|
+//       WakeUp Labs 2022         
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
